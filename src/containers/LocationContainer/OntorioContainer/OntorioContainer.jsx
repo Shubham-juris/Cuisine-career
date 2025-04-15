ba7067ddd53b7b3e99bde7db5e5dec96759fb8bd
navjot kaur <navu77196@gmail.com>
2025-04-15 11:37:16 +0530

    location/pages

---
+++ src/containers/LocationContainer/OntorioContainer/OntorioContainer.jsx
@@ -0,0 +1,16 @@
+import Footer from '@/components/Footer'
+import OntorioSection from '@/components/LocationComponents/OntorioSection'
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const OntorioContainer = () => {
+  return (
+    <div>
+        <Navbar/>
+      <OntorioSection/>
+      <Footer/> 
+    </div>
+  )
+}
+
+export default OntorioContainer
---
+++ src/containers/LocationContainer/QuebeeContainer/QuebeeConatiner.jsx
@@ -0,0 +1,16 @@
+import Footer from '@/components/Footer'
+import Quebee from '@/components/LocationComponents/Quebee'
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const QuebeeConatiner = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Quebee/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default QuebeeConatiner
